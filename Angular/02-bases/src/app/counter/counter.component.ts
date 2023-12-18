211b58559a621c46ddcebc90186e313bfdb91e60
SergioCalde <sergio.calderon.dm@outlook.com>
2023-12-18 00:09:27 -0600

    Start with angular bases review

---
+++ Angular/02-bases/src/app/counter/counter.component.ts
@@ -0,0 +1,17 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-counter',
+    templateUrl: './counter.component.html'
+})
+export class CounterComponent {
+    public counter: number = 10;
+
+    increaseBy(value: number): void {
+    this.counter += value;
+    }
+
+    resetCounter(){
+    this.counter = 10;
+    }
+}
