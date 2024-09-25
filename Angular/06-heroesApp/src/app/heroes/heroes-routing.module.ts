e4bca6f45c1c317d1395e47161f0f8b6830c4f6d
SergioCalde <sergio.calderon.dm@outlook.com>
2024-09-24 23:43:48 -0600

    feat: add routes to the modules

--- Angular/06-heroesApp/src/app/heroes/heroes-routing.module.ts
+++ Angular/06-heroesApp/src/app/heroes/heroes-routing.module.ts
@@ -1,7 +1,43 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { NewHeroPageComponent } from './pages/new-hero-page/new-hero-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutPageComponent,
+    children: [
+      {
+        path: 'new-hero',
+        component: NewHeroPageComponent,
+      },
+      {
+        path: 'search',
+        component: SearchPageComponent,
+      },
+      {
+        path: 'edit/:id',
+        component: NewHeroPageComponent,
+      },
+      {
+        path: 'list',
+        component: ListPageComponent,
+      },
+      { // Scalderon: Wildcard page, it should always come last; otherwise, the other pages would not be accessible.
+        path: ':id',
+        component: HeroPageComponent,
+      },
+      { 
+        path: '**',
+        redirectTo: 'list',
+      },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
