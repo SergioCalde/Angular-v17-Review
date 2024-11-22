5f99244f33e053997206c8b39547918ae01ed9e9
SergioCalde <sergio.calderon.dm@outlook.com>
2024-11-22 11:08:30 -0600

    chore: add auth guards to git

---
+++ Angular/06-heroesApp/src/app/auth/guards/public.guard.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { CanMatch, CanActivate, Route, UrlSegment, ActivatedRouteSnapshot,RouterStateSnapshot, Router } from '@angular/router';
+import { map, Observable, tap } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class PublicGuard implements CanMatch, CanActivate {
+
+    constructor( 
+        private authService: AuthService,
+        private router: Router
+    ) { }
+    
+
+    private checkAuthStatus():boolean | Observable<boolean> {
+        return this.authService.checkAuthentication()
+        .pipe(
+            tap( isAuthenticated => {
+                if( isAuthenticated ) this.router.navigate(['./']);
+            }),
+            map( isAuthenticated => !isAuthenticated )
+        );
+    }
+
+    canMatch(route: Route, segments: UrlSegment[]): boolean | Observable<boolean> {
+        return this.checkAuthStatus();
+    }
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> {
+        return this.checkAuthStatus();
+    }
+
+}
