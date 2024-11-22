4e7e7b32045cf44cfe215a51cd9995f2a3bdf1a3
SergioCalde <sergio.calderon.dm@outlook.com>
2024-11-22 11:07:18 -0600

    feat: Practice Guards works as spected. Finish Guards Module

--- Angular/06-heroesApp/src/app/app.component.ts
+++ Angular/06-heroesApp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth/services/auth.service';
 
 @Component({
   selector: 'app-root',
--- Angular/06-heroesApp/src/app/auth/services/auth.service.ts
+++ Angular/06-heroesApp/src/app/auth/services/auth.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environments';
 import { User } from '../interfaces/user.interface';
-import { Observable, tap } from 'rxjs';
+import { catchError, map, Observable, of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -24,6 +24,20 @@ export class AuthService {
       .pipe(
         tap( user => { this.user = user; }),
         tap( user => {localStorage.setItem('token', JSON.stringify( user.id+'.'+user.email+'.123dsfsd312ed' ));}),
+      );
+  }
+
+  checkAuthentication(): Observable<boolean>{
+
+    if ( !localStorage.getItem('token') ) return of(false);
+
+    const token = localStorage.getItem('token');
+
+    return this.http.get<User>(`${ this.baseUrl }/users/1`)
+      .pipe(
+        tap( user => { this.user = user; }),
+        map( user => !!user),
+        catchError( error => of(false) )
       );
   }
 
