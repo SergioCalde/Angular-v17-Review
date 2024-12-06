d9618c4a8e65b09b953fc73bc39e67baef7ee3e6
SergioCalde <sergio.calderon.dm@outlook.com>
2024-12-05 21:06:38 -0600

    chore: Start section 20: Angular LifeCycle

---
+++ Angular/09-lifecycle/src/app/products/pages/product/product.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'products-product-page',
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.css'
+})
+export class ProductComponent {
+
+}
