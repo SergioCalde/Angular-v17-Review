9dadae30d48af34c84fd0f84f3c584a488111431
SergioCalde <sergio.calderon.dm@outlook.com>
2024-12-16 23:12:16 -0600

    feat: Finish section 23, standalone components.

--- Angular/10-mapsApp/src/app/maps/maps.module.ts
+++ Angular/10-mapsApp/src/app/maps/maps.module.ts
@@ -7,18 +7,18 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoic2NkbTk3IiwiYSI6ImNsNnp0NTE3MzAwbTQzbnBhaGlwZ
 
 import { MapsRoutingModule } from './maps-routing.module';
 import { MiniMapComponent } from './components/mini-map/mini-map.component';
-import { SideMenuComponent } from './components/side-menu/side-menu.component';
 import { MapsLayoutComponent } from './layout/maps-layout/maps-layout.component';
 import { FullScreenPageComponent } from './pages/full-screen-page/full-screen-page.component';
 import { MarkersPageComponent } from './pages/markers-page/markers-page.component';
 import { PropertiesPageComponent } from './pages/properties-page/properties-page.component';
 import { ZoomRagePageComponent } from './pages/zoom-rage-page/zoom-rage-page.component';
+import { CounterAloneComponent } from '../alone/components/counter-alone/counter-alone.component';
+import { SideMenuComponent } from '../alone/components/side-menu/side-menu.component';
 
 
 @NgModule({
   declarations: [
     MiniMapComponent,
-    SideMenuComponent,
     MapsLayoutComponent,
     FullScreenPageComponent,
     MarkersPageComponent,
@@ -27,7 +27,9 @@ import { ZoomRagePageComponent } from './pages/zoom-rage-page/zoom-rage-page.com
   ],
   imports: [
     CommonModule,
-    MapsRoutingModule
+    MapsRoutingModule,
+    CounterAloneComponent,
+    SideMenuComponent,
   ]
 })
 export class MapsModule { }
